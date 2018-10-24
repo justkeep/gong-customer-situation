bcce843b64dc86fdca972858e15f98c21541aa4e
coder_gyk <www.2601720146@qq.com>
2018-10-24 21:53:06 +0800

    first commit

---
+++ src/main/java/com/gong/CustomerSituationApplication.java
@@ -0,0 +1,12 @@
+package com.gong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSituationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSituationApplication.class, args);
+	}
+}
