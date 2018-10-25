db3a5b9e42fb4f811002d1d316c5da04e1d9cc4b
coder_gyk <www.2601720146@qq.com>
2018-10-25 13:57:02 +0800

    1.0.0-feature:搭建项目

--- gong-customer-situation-api/src/main/java/com/gong/CustomerSituationApiApplication.java
+++ gong-customer-situation-api/src/main/java/com/gong/CustomerSituationApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerSituationApplication {
+public class CustomerSituationApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerSituationApplication.class, args);
+		SpringApplication.run(CustomerSituationApiApplication.class, args);
 	}
 }
--- gong-customer-situation-api/src/test/java/com/gong/CustomerSituationApiApplicationTests.java
+++ gong-customer-situation-api/src/test/java/com/gong/CustomerSituationApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomerSituationApplicationTests {
+public class CustomerSituationApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
